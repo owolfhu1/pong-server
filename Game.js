97fd45d6bf5834a4458c246af5f9e9985b017af9
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2018-08-08 23:46:53 -0400

    first online attempt

--- Game.js
+++ Game.js
@@ -1,2 +1,94 @@
 
 
+
+function Game(leftSocketEmit, rightSocketEmit) {
+
+    const state = {
+        direction : {
+            x:Math.round((Math.random())) === 0 ? 2 : -2,
+            y:Math.round((Math.random())) === 0 ? 1 : -1,
+        },
+        x: 300,
+        y: 200,
+        left : 150,
+        right : 150,
+        intervals : []
+    };
+    
+    this.getState = () => {
+        let {left, right, x, y} = state;
+        return {left, right, x, y};
+    };
+    
+    const stop = () => {
+        for (let i in state.intervals)
+            clearInterval(state.intervals[i]);
+        state.intervals = [];
+    };
+    
+    const paddleIncrement = () => {
+        let increment = 20 + state.intervals.length * 3;
+        increment = increment > 80 ? 80 : increment;
+        return increment;
+    };
+    
+    this.moveRightPaddle = string => {
+        if (string === 'ArrowUp' && state.right < 300)
+            state.right = state.right + paddleIncrement();
+        else if (state.right > 0)
+            state.right = state.right - paddleIncrement();
+        state.right = state.right < 0 ? 0 : state.right > 300 ? 300 : state.right;
+    };
+    
+    this.moveLeftPaddle = string => {
+        if (string === 'ArrowUp' && state.left < 300)
+            state.left = state.left + paddleIncrement();
+        else if (state.left > 0)
+            state.left = state.left - paddleIncrement();
+        state.left = state.left < 0 ? 0 : state.left > 300 ? 300 : state.left;
+        
+    };
+    
+    const flipX = () => {
+        state.direction.x = state.direction.x * -1;
+        state.intervals.push(setInterval(moveBall,80));
+    };
+    
+    const moveBall = () => {
+        state.x += state.direction.x;
+        state.y += state.direction.y;
+    
+        if (state.y < 0) {
+            state.direction.y = state.direction.y * -1;
+            state.y = state.direction.y;
+        } else if (state.y > 400) {
+            state.direction.y = state.direction.y * -1;
+            state.y = 400 + state.direction.y;
+        }
+        if (state.x < 0) {
+            if (state.y < state.left - 4 || state.y > state.left + 100)
+                stop();
+            else {
+                let spot = state.y - state.left;
+                state.direction.y = spot < 20 ? -3 : spot > 80 ? 3 : state.direction.y > 0 ? 1 : -1;
+                flipX();
+            }
+        }
+        if (state.x > 600) {
+            if (state.y < state.right - 4 || state.y > state.right + 100)
+                stop();
+            else {
+                let spot = state.y - state.right;
+                state.direction.y = spot < 20 ? -3 : spot > 80 ? 3 : state.direction.y > 0 ? 1 : -1;
+                flipX();
+            }
+        }
+    };
+    
+    this.start = () => state.intervals.push(setInterval(moveBall,40));
+    
+}
+
+
+
+module.exports = Game;
--- server.js
+++ server.js
@@ -6,12 +6,42 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const userMap = {};
-const lobby = [];
-const gameMap = {};
+const Game = require('./Game');
 
-const startGame = (leftPlayer,rightPlayer) => {
-    //TODO
+const userMap = {}; //{username : socketId}
+const lobby = []; //[userInLobby,secondUserInLobby, ...]
+const gameMap = {}; //{gameId : gameObj}
+const gameIdMap = {}; //{username : gameId}
+
+function GameObj(game,leftPlayer,rightPlayer) {
+    this.game = game;
+    this.leftPlayer = leftPlayer;
+    this.rightPlayer = rightPlayer;
+    this.id = 'gameId' + Math.random().toString(36).substr(2, 6);
+    this.interval = null;
+    io.to(userMap[leftPlayer]).emit('start_game');
+    io.to(userMap[rightPlayer]).emit('start_game');
+}
+
+const makeGame = (leftPlayer,rightPlayer) => {
+    let game = new Game(io.to(userMap[leftPlayer]).emit,io.to(userMap[rightPlayer]).emit);
+    let gameObj = new GameObj(game,leftPlayer,rightPlayer);
+    gameIdMap[leftPlayer] = gameObj.id;
+    gameIdMap[rightPlayer] = gameObj.id;
+    gameMap[gameObj.id] = gameObj;
+    lobby.splice(lobby.indexOf(leftPlayer),1);
+    lobby.splice(lobby.indexOf(rightPlayer),1);
+    for (let i in lobby)
+        io.to(userMap[lobby[i]]).emit('lobby', lobby);
+};
+
+const startGame = gameObj => {
+  
+    gameObj.game.start();
+    gameObj.interval = setInterval(() => {
+        io.to(userMap[gameObj.leftPlayer]).emit('update_game',gameObj.game.getState());
+        io.to(userMap[gameObj.rightPlayer]).emit('update_game',gameObj.game.getState());
+    },100);
 };
 
 io.on('connection', socket => {
@@ -42,7 +72,7 @@ io.on('connection', socket => {
     
     socket.on('accept', name => {
         if (lobby.indexOf(name) !== -1)
-            startGame(username,name);
+            makeGame(username,name);
         else socket.emit('chat', `SYSTEM: ${name} is no longer in the lobby.`);
     });
     
@@ -51,6 +81,22 @@ io.on('connection', socket => {
             io.to(userMap[name]).emit('chat', `SYSTEM: ${username} has declined your invitation.`);
     });
 
+    socket.on('move_paddle', string => {
+        let gameObj = gameMap[gameIdMap[username]];
+        if (gameObj.leftPlayer === username)
+            gameObj.game.moveLeftPaddle(string);
+        else
+            gameObj.game.moveRightPaddle(string);
+        io.to(userMap[gameObj.leftPlayer]).emit('update_game', gameObj.game.getState());
+        io.to(userMap[gameObj.rightPlayer]).emit('update_game', gameObj.game.getState());
+    });
+    
+    socket.on('start_game', () => {
+        let gameObj = gameMap[gameIdMap[username]];
+        if (!gameObj.interval)
+            startGame(gameObj);
+        
+    });
 
     socket.on('disconnect', () => {
 
